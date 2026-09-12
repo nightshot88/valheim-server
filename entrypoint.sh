80549382d692fb856e48c86d5fe8783b63c565b6
nightshot88 <markus.p.dertinger@googlemail.com>
2026-09-12 12:29:39 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo "=== Prüfe/installiere aktuellste Valheim-Version ==="
+steamcmd +force_install_dir ${GAME_DIR} \
+         +login anonymous \
+         +app_update 896660 validate \
+         +quit
+
+echo "=== Starte Valheim Dedicated Server ==="
+
+cd ${GAME_DIR}
+exec ./valheim_server.x86_64 -name "${SERVER_NAME}" \
+     -port ${SERVER_PORT} \
+     -world "${WORLD_NAME}" \
+     -password "${SERVER_PASSWORD}" \
+     -crossplay ${CROSSPLAY:-false} \
+     -public ${PUBLIC:-1}
