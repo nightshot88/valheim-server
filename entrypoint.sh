e1124b82fb8ebd4067425075f9873ea2068726fe
nightshot88 <markus.p.dertinger@googlemail.com>
2026-09-13 11:47:00 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 set -e
 
-echo "=== Prüfe/installiere aktuellste Valheim-Version ==="
-steamcmd +force_install_dir ${GAME_DIR} \
+echo "=== Setting up permissions ==="
+
+# Fix permissions on mounted volumes if they exist
+if [ -d "/data" ]; then
+    chown -R valheim:valheim /data || true
+fi
+
+# Create necessary directories
+mkdir -p /home/valheim/.config/unity3d/IronGate/Valheim
+chown -R valheim:valheim /home/valheim/.config
+
+echo "=== Checking/Installing Valheim version ==="
+steamcmd +force_install_dir /home/valheim/valheim-server \
          +login anonymous \
          +app_update 896660 validate \
          +quit
 
-echo "=== Starte Valheim Dedicated Server ==="
+echo "=== Starting Valheim Dedicated Server ==="
 
-cd ${GAME_DIR}
-exec ./valheim_server.x86_64 -name "${SERVER_NAME}" \
+cd /home/valheim/valheim-server
+exec su -c './valheim_server.x86_64 -name "${SERVER_NAME}" \
      -port ${SERVER_PORT} \
      -world "${WORLD_NAME}" \
      -password "${SERVER_PASSWORD}" \
      -crossplay ${CROSSPLAY:-false} \
-     -public ${PUBLIC:-1}
+     -public ${PUBLIC:-1}' valheim
